fc70829cf4ecd8606804121a7e48e6bcc8efe930
Katherine Lewis <kale8432@colorado.edu>
2019-08-24 16:21:07 -0600

    fetch

--- src/reducers/palettesReducer.js
+++ src/reducers/palettesReducer.js
@@ -1,6 +1,8 @@
 export const palettesReducer = (state = [], action) => {
   const { type, payload } = action;
   switch (type) {
+    case 'ADD_PALETTES':
+    return [...state, ...payload.palettes]
     case 'ADD_PALETTE':
     return [...state, payload.newPalette]
     case "DELETE_PALETTE":
