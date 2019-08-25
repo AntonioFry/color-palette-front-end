9ebce84f0f96b1dc73a07d66fdf299781968cfa6
Antonio Fry <antoniofry2000@gmail.com>
2019-08-24 22:24:34 -0600

    setup PaletteForm component

---
+++ src/components/PaletteForm/PaletteForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react-redux';
+import { addPalette } from '../../actions';
+
+export class PaletteForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      paletteName: '',
+      projectName: '',
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    const projectNames = this.props.projects.map(project => {
+      return <option>{project.name}</option>
+    })
+    return (
+      <form>
+        <select
+        value={this.state.projectName}
+        name='projectName'
+        onSelect={(e) => this.handleChange(e)}>
+          {projectNames}
+        </select>
+        <input
+          value={this.state.paletteName}
+          name="paletteName"
+          placeholder="Name Palette"
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button onClick={(e) => this.handleSubmit}>Submit</button>
+      </form>
+    )
+  }
+}
+
+export const mapStateToProps = (store) => ({
+  projects: store.projects
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  addPalette: palette => dispatch(addPalette(palette))
+})
+
+export default PaletteForm;
