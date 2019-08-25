296e43be1569a11318d9adb4cef81f6552cbb921
Antonio Fry <antoniofry2000@gmail.com>
2019-08-25 13:00:24 -0600

    setup project and palette components

---
+++ src/components/Palette/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Palette = () => {
+  return (
+    <div>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+    </div>
+  )
+}
---
+++ src/components/Project/Project.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Project = ({ projectName }) => {
+  return (
+    <div>
+      <h3>{projectName}</h3>
+
+    </div>
+  )
+}
