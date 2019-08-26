d764d24b468e02a02eeabe7ad7008b567025d490
Katherine Lewis <kale8432@colorado.edu>
2019-08-25 22:00:25 -0600

    get requests fetch from backend

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,14 @@ import { getProjects, getPalettes } from '../../Utils/API/apiCalls';
 import './App.css';
 import ProjectForm from '../ProjectForm/ProjectForm';
 import { connect } from 'react-redux'
-import { addProject, addPalette } from '../../actions';
+import { addProjects, addPalettes } from '../../actions';
+import GeneratedColors from '../GeneratedColors/GeneratedColors';
 
 export class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      loading: false
     }
   }
   
@@ -17,7 +18,8 @@ export class App extends Component {
     try {
       const projects = await getProjects();
       const palettes = await getPalettes();
-
+      this.props.addProjects(projects)
+      this.props.addPalettes(palettes)
     } catch (error) {
       throw new Error(`failed to fetch: ${error.message}`)
     }
@@ -29,21 +31,23 @@ export class App extends Component {
         <header>
           <h1>Palette Picker</h1>
         </header>
+        {/* need to create loading comp and change based on api  */}
         <ProjectForm/>
+        <GeneratedColors/>
       </main>
     )
   }
 }
 
-const mapStateToProps = () => ({
+const mapStateToProps = ({ projects, palettes }) => ({
   projects,
   palettes
 })
 
-const mapDispatchToProps = dispatch => {
-  addProject(project => dispatch(addProject(project))),
-  addPalette(palette => dispatch(addPalette(palette)))
-}
+const mapDispatchToProps = dispatch => ({
+  addProjects: (projects => dispatch(addProjects(projects))),
+  addPalettes: (palettes => dispatch(addPalettes(palettes)))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
 
--- src/components/ProjectForm/ProjectForm.js
+++ src/components/ProjectForm/ProjectForm.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {addProject} from '../../actions';
 
 export class ProjectForm extends Component {
+
+
+// handleSubmit = (e) => {
+//   e.preventDefault()
+//   // const project = {
+//   //   name: 
+//   // }
+//   addProject(project)
+// }
+//how to pass project?
   render() {
     return (
-      <div>
-        <label for="projectInput">Project Name</label>
-        <input />
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label for="project">Project Name</label>
+        <input type="text" placeholder="name your project" name="project" onChange={this.handleChange}/>
+        <input type="submit" value="create project"/>
+      </form>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  
+const mapStateToProps = ({project}) => ({
+  project
 })
 
-const mapDispatchToProps = {
-  
-}
+const mapDispatchToProps = dispatch => ({
+  addProject: (project => dispatch(addProject(project)))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectForm)
