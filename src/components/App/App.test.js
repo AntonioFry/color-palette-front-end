896faf9058fcb747b9060fff803444daec89cf5b
Antonio Fry <antoniofry2000@gmail.com>
2019-08-27 16:24:34 -0600

    setup app tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,4 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App.js';
+import { shallow } from 'enzyme';
+import { mapStateToProps, mapDispatchToProps } from './App';
+import { addProjects, addPalettes } from '../../actions';
 
+describe('App', () => {
+
+  describe('App component',  () => {
+    let wrapper;
+    let props;
+
+    beforeEach(() => {
+      props = {
+        palettes: [{}, {}]
+      }
+      wrapper = shallow(<App palettes={props.palettes} />)
+    });
+
+    it('should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  
+  })
+})
