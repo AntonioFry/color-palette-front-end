dea2548510abada01f2004e7f944365670e07ba5
Antonio Fry <antoniofry2000@gmail.com>
2019-08-26 21:22:57 -0600

    render and style projects and palettes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,14 +1,15 @@
 import Palette from '../Palette/Palette';
 import React from 'react'
+import './Project.css'
 
 export default function Project(props) {
   const paletteCards = props.palettes.map(palette => {
     return <Palette {...palette} />
   })
   return (
-    <div>
-      <h2>{props.name}</h2>
+    <article className="project">
+      <h2 className="project-name">{props.name}</h2>
       {paletteCards}
-    </div>
+    </article>
   )
 }
