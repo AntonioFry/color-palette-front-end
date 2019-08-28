64bbb48001ccba50f88863439153736b76865a3e
Katherine Lewis <kale8432@colorado.edu>
2019-08-27 23:36:54 -0600

    adjusted margin and height on palette comp colors

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,15 +9,14 @@ export function Project(props) {
   const paletteCards = props.palettes.map(palette => {
     return <Palette {...palette} />
   })
-  // const paletteMessage = props.palettes.length = 0 ? <p>No Palettes yet</p> : {paletteCards}
 
   return (
     <article className="project">
       <div className="project-name-container">
           <h2 className="project-name">{props.name}</h2>
-          <button className="rmvBtn"onClick={() => deleteProject(props.id, props.removeProject(props.id))}>X</button>
+          <button className="rmvBtn"onClick={() => deleteProject(props.id, props.removeProject(props.id))}><i class="far   fa-trash-alt"></i></button>
       </div>
-     {/* {paletteMessage} */}
+     {!props.palettes.length ? <p className="noPalettesMessage">No Palettes Yet</p> : paletteCards}
     </article>
   )
 }
