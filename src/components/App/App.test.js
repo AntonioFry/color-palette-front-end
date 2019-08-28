5b8e40e9d7b5fa8b7dd46431e16b5c1b6ee11674
Antonio Fry <antoniofry2000@gmail.com>
2019-08-27 19:45:47 -0600

    create test for mapStateToProps

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -20,6 +20,50 @@ describe('App', () => {
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot();
     });
+
+    it.skip('should throw an error if componentDidMount cathces an error', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      });
+    
+      expect(wrapper.instance().componentDidMount()).toEqual()
+    })
   
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return array with all palettes', () => {
+      const mockStore = {
+        palettes: [
+          { id: 1, palette_name: 'Dog Colors' },
+          { id: 2, palette_name: 'Cat Colors' },
+          { id: 3, palette_name: 'Something' }
+        ]
+      }
+
+      const expected =  {
+        palettes:  [
+          { id: 1, palette_name: 'Dog Colors' },
+          { id: 2, palette_name: 'Cat Colors' },
+          { id: 3, palette_name: 'Something' }
+        ] 
+    }
+
+      const mappedProps = mapStateToProps(mockStore);
+      expect(mappedProps).toEqual(expected);
+    });
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with addProjects', () => {
+      
+       
+    });
+
+    it('should call dispatch with addPalettes', () => {
+
+    });
   })
 })
